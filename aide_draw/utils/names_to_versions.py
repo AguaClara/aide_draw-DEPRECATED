f404d6d5807f3ebe0a6604b19655d131b68f5248
matanp <mgp64@cornell.edu>
2018-05-07 21:30:03 -0400

    more documentation

--- aide_draw/utils/names_to_versions.py
+++ aide_draw/utils/names_to_versions.py
@@ -1,18 +1,22 @@
-import adsk.core, adsk.fusion, traceback
-import sys, os
-import json
+import adsk.core, adsk.fusion
 
-from .. import yaml
-
-def build_names_to_versions_helper(occurrences, input_dict):
+def build_names_to_versions_helper(occurrences, input_dict):    
     """
     Helper used to build dictionary mapping component names used in
     parameters file to component names used in Fusion 360, e.g.
     testAssem -> testAssem v14
 
-    :param occurrences: Occurrences object used to access child occurrences
-    :param input_dict: Python dict to update with new key-value pairs
-    :return: The completed mapping of param component names to Fusion component names
+    Args:
+        occurrences: Occurrences object used to access child occurrences
+        input_dict: Python dict to update with new key-value pairs
+
+    Returns:
+        Dictionary containing the completed mapping of param component names to Fusion component names
+
+    Raises:
+
+    Examples:
+        ?
     """
     for i in range(0, occurrences.count):
         occ = occurrences.item(i)
@@ -21,7 +25,7 @@ def build_names_to_versions_helper(occurrences, input_dict):
         name_only = name_parts[0]
         input_dict[name_only] = full_name
 
-        if occ.childOccurrences:
+        if occ.childOccurrences: #if there are more levels of assemblies to recurse through
             child_dict = build_names_to_versions_helper(occ.childOccurrences, input_dict)
             child_copy = child_dict.copy()
             input_dict.update(child_copy)
@@ -29,13 +33,21 @@ def build_names_to_versions_helper(occurrences, input_dict):
     return input_dict
 
 
-def build_names_to_versions(root_component):
+def build_names_to_versions(root_component):    
     """
     Builds a mapping from parameter file component names to versioned Fusion 360
     component names, e.g. testAssem -> testAssem v14
 
-    :param root_component: Root component of Fusion 360 assembly
-    :return: Dictionary mapping parameter component names to Fusion 360 component names
+    Args:
+        root_component: Root component of Fusion 360 assembly
+
+    Returns:
+        Dictionary mapping parameter component names to Fusion 360 component names
+
+    Raises:
+
+    Examples:
+        ?
     """
     input_dict = {}
     full_name = root_component.name
--- aide_draw/utils/yaml_compare.py
+++ aide_draw/utils/yaml_compare.py
@@ -1,33 +1,51 @@
 from .. import yaml
 
 
-#compares yaml files, ignoring hp attributes
 def compare_yamls(path1, path2):
-	f1 = open(path1, 'r')
-	f2 = open(path2, 'r')
+    """
+    Compare two yamls. Return true if they are the same excluding hp attributes
 
-	y1 = yaml.load(f1)
-	y2 = yaml.load(f2)
+    Args:
+        path1: path to a yaml file
+        path2: path to a 2nd yaml file
 
-	f1.close()
-	f2.close()
-	
-	return compare_yamls_helper(y1, y2)
+    Returns:
+        Boolean representing whether the two yamls were deemed equal
+
+    Raises:
+
+    Examples:
+        ?
+    """
+    f1 = open(path1, 'r')
+    f2 = open(path2, 'r')
+    
+    y1 = yaml.load(f1)
+    y2 = yaml.load(f2)
+    
+    f1.close()
+    f2.close()
+    	
+    return compare_yamls_helper(y1, y2)
 
 
 
 def compare_yamls_helper(y1, y2):
+        #starts as True, set to False if any incongruency found
 	equal = True
 
+        #regular equality if strings
 	if isinstance(y1, str):
 		return y1 == y2
 
 	for key, value in y1.items():
 		if key in y2:
+                  #equal if compare_yamls_helper is always true for all keys
 			equal = equal and compare_yamls_helper(y1[key], y2[key])
 		elif key == "hp" or key == "cp":
 			continue
 		else:
+                  #key missing from 2nd yaml, return false
 			return False
 
 	return equal
